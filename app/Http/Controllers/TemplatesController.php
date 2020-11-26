bcd36e8dea6e6133a4e55946a512fad2dd739501
Windzor De Guzman <wdavedeguzman@gmail.com>
2020-11-26 14:14:27 -0800

    layouts

---
+++ app/Http/Controllers/TemplatesController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TemplatesController extends Controller
+{
+
+	public function index() 
+	{ 
+
+		 return view('templates.index');
+	}
+   
+}
